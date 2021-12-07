160c7ad92228910f12914c93968d692ff5a3ee54
wangzhitao <wangzhitao@co-mall.com>
2021-12-07 16:15:35 +0800

    feat: home

--- entry/src/main/js/default/pages/home/home.js
+++ entry/src/main/js/default/pages/home/home.js
@@ -1,6 +1,8 @@
 
 export default {
     data: {
-        title: 'World'
+        title: 'World',
+        subsiteName:'北京渔阳置业大厦店',
+        imageAdBanner:'https://puzhehei.oss-cn-beijing.aliyuncs.com/global/image-ad-banner.png'
     }
 }
